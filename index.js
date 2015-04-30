d89b38a6f2288f9ffdf0695c72b6380fb500e3ed
josephyearsley <joggino23@gmail.com>
2015-04-30 23:36:57 +0100

    Started to turn it into a node handler for variety, to add the ability to save the output.

---
+++ index.js
@@ -0,0 +1,26 @@
+var childProcess = require('child_process'),
+    ls,
+    argv = require("node-argument-parser").parse("./arguments.json", process),
+    fs = require('fs');
+
+console.dir(argv);
+console.log('Arguments:', process.argv.length);
+
+ls = childProcess.exec('mongo ' + argv.database + ' --eval "var collection = \''+ argv.collection + '\'; var plugins=\'' +
+                                argv.plugin +'|numColumns=' + argv.num + '\';" ' +  argv.jsfile + ' -quiet'
+, function (error, stdout, stderr) {
+    if (error) {
+        console.log(error.stack);
+        console.log('Error code: '+error.code);
+        console.log('Signal received: '+error.signal);
+    }
+    console.log('Child Process STDOUT: '+stdout);
+    console.log('Child Process STDERR: '+stderr);
+    fs.writeFile(argv.file, stdout, function(err){
+            console.log(err);
+    });
+});
+
+ls.on('exit', function (code) {
+    console.log('Child process exited with exit code '+code);
+});
